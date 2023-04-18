c2b89bed988144dd735a668d689199b6b23eb52e
ImKunYoung <ur2kunyoung2@outlook.com>
2023-04-18 12:18:15 +0900

    feat: Add Padding

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -33,13 +33,14 @@ class _HomeScreenState extends State<HomeScreen> {
             'Welcome to My Quiz App',
             style: TextStyle(fontSize: width * 0.065, fontWeight: FontWeight.bold),
           ),
-          Text(
+          const Text(
             '퀴즈를 풀기 전 안내사항입니다. \n 꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
             textAlign: TextAlign.center,),
           Padding(padding: EdgeInsets.all(width * 0.048)),
           _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
           _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤 \n 다음 문제 버튼을 눌러주세요. '),
           _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+          Padding(padding: EdgeInsets.all(width * 0.048)),
         ],
       ),
     ));
