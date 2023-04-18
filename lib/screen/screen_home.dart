b7eeb98d850d75d8b3e516b914881df1072ba446
ImKunYoung <ur2kunyoung2@outlook.com>
2023-04-18 12:16:07 +0900

    feat: Add _buildStep

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -37,8 +37,29 @@ class _HomeScreenState extends State<HomeScreen> {
             '퀴즈를 풀기 전 안내사항입니다. \n 꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
             textAlign: TextAlign.center,),
           Padding(padding: EdgeInsets.all(width * 0.048)),
+          _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+          _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤 \n 다음 문제 버튼을 눌러주세요. '),
+          _buildStep(width, '3. 만점을 향해 도전해보세요!'),
         ],
       ),
     ));
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(width * 0.048, width * 0.024, width * 0.048, width * 0.024),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(padding: EdgeInsets.only(right: width * 0.024)),
+          Text(title),
+        ],
+      ),
+
+    );
   }
 }
